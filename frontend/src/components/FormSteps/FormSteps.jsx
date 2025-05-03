a5229478b17d972f922463b938846dd0c6606625
Rafael Souza <rafaelsprata@outlook.com>
2025-05-03 18:09:56 -0300

    feat: finalização da tela de cadastro

--- frontend/src/components/FormSteps/FormSteps.jsx
+++ frontend/src/components/FormSteps/FormSteps.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 import './FormSteps.css'
 
-function FormSteps() {
+function FormSteps({step}) {
   return (
     <div className='FormSteps'>
         <div className='container-steps'>
-          <div className="steps on"></div>
-          <div className="steps off"></div>
-          <div className="steps off"></div>
+          <div className={`steps ${step == 1 ? 'on' : 'off'}`}></div>
+          <div className={`steps  ${step == 2 ? 'on' : 'off'}`}></div>
+          <div className={`steps  ${step == 3 ? 'on' : 'off'}`}></div>
         </div>
         <span className='line'>----------------------</span>
     </div>
