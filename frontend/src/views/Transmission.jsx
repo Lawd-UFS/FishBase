83287d9b310645329b2c20ad5eb0e6511d3e018c
Rafael Souza <rafaelsprata@outlook.com>
2025-05-08 20:41:46 -0300

    tela de transmissão finalizada

---
+++ frontend/src/views/Transmission.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import TransmissionContainer from '../components/TransmissionContainer/TransmissionContainer';
+
+const Transmission = () => {
+    return (
+        <TransmissionContainer></TransmissionContainer>
+    );
+};
+
+export default Transmission;
