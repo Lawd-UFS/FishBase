b83fb52e333c918609601444ac33fcc647051c52
danieljstri <euft6y@gmail.com>
2025-05-09 01:58:53 -0300

    feat: create about section in home page

--- frontend/src/views/Homepage/HomePage.jsx
+++ frontend/src/views/Homepage/HomePage.jsx
@@ -21,15 +21,18 @@ const HomePage = () => {
 
     return (
         <div className="content">
-            <section className="title">
-                <h1>23rd FishBase and SeaLifeBase Symposium</h1>
+            <section className="hero">
+                <div className="title">
+                    <h1>23rd FishBase and SeaLifeBase Symposium</h1>
+                </div>
+                <button className="register-button">
+                    <Link to="/register" className="register-link">Inscreva-se</Link>
+                </button>
             </section>
-            <button className="register-button">
-                <Link to="/register" className="register-link">Inscreva-se</Link>
-            </button>
             <div className="hero-footer">
                 <h2>LINKING NORTH-SOUTH AND EAST-WEST E DO FISHBASE CONSORTIUM ANNUAL MEETING</h2>
             </div>
+            
             <section className="schedule">
                 <h2>Programação</h2>
                 <div className="cards">
@@ -38,6 +41,17 @@ const HomePage = () => {
                         <ScheduleCard key={item.id} data={item} />
                     ))}
                 </div>
+            </section>
+            <section className="about-event">
+                <h2>Sobre o evento</h2>
+                <section className="about-event-content">
+                    <div className="map-event">
+                        <img src="./images/mapa-mundi.png" alt="Mapa Mundi" />  
+                    </div>
+                    <p>
+                        O FishBase e o SeaLifeBase são bancos de dados globais de informações sobre peixes e vida marinha, respectivamente. Eles são amplamente utilizados por pesquisadores, conservacionistas e gestores de recursos marinhos em todo o mundo. O FishBase é um banco de dados abrangente que contém informações sobre mais de 34.000 espécies de peixes, incluindo dados sobre sua biologia, ecologia, distribuição geográfica e status de conservação. O SeaLifeBase é um banco de dados semelhante, mas abrange uma gama mais ampla de organismos marinhos, incluindo invertebrados e plantas marinhas.
+                    </p>
+                </section>  
             </section>
         </div>
     );
