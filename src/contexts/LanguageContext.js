af324b821236c962ac48e9e255673ee05502173f
Davi Bittencourt <dbittencourt2002@gmail.com>
2025-05-19 23:29:37 -0300

    refactor: modifica import para utilizar alias

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,10 +1,10 @@
 'use client';
 
-import React, { createContext, useContext, useState, useEffect } from 'react';
-import pt from '../locales/pt.json';
-import en from '../locales/en.json';
+import { createContext, useContext, useState, useEffect } from 'react';
+import pt from '@/locales/pt.json';
+import en from '@/locales/en.json';
 
-const LanguageContext = createContext();
+const LanguageContext = createContext(null);
 
 const languages = { pt, en };
 
