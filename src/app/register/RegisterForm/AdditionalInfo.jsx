5bacbe21d2b05d99f8c815d638fec794952a41f3
Davi Bittencourt <dbittencourt2002@gmail.com>
2025-05-20 14:45:28 -0300

    feat(form): adiciona submissão de formulário através da api

--- src/app/register/RegisterForm/AdditionalInfo.jsx
+++ src/app/register/RegisterForm/AdditionalInfo.jsx
@@ -1,24 +1,28 @@
 import { useLanguage } from '@/contexts/LanguageContext';
-import styles from '@/components/RegisterFormStage/RegisterFormStage.module.css';
-import { Form } from '../Form';
+import { Form } from '@/components/Form';
 import { LaptopMinimal, MapPin } from 'lucide-react';
+import formStyles from '@/components/Form/Form.module.css';
 
-export default function AdditionalInfo() {
+export default function AdditionalInfo({ styles, formFunctions }) {
+  if (!formFunctions) return null;
+
   const { texts } = useLanguage();
+
+  const { register, errors } = formFunctions;
 
   const modalityIcons = {
     'in-person': <MapPin />,
     remote: <LaptopMinimal />,
   };
 
-  const genderOptions = Object.entries(texts.register.genderOptions).map(
-    ([key, value]) => {
+  const genderOptions = Object.entries(texts.register.genderOptions)
+    .reverse()
+    .map(([key, value]) => {
       return {
         value: key,
         label: value,
       };
-    }
-  );
+    });
 
   const modalityOptions = Object.entries(texts.register.modalityOptions).map(
     ([key, value]) => {
@@ -31,43 +35,54 @@ export default function AdditionalInfo() {
   );
 
   // Remove a primeira opção que no caso é o placeholder
-  genderOptions.shift();
+  genderOptions.pop();
 
   return (
-    <section className={styles.stage}>
+    <form className={styles.stage}>
       <Form.Input
         label={texts.register.fields.institution}
         type='text'
         name='institution'
+        {...register('institution')}
+        error={errors.institution?.message}
       />
-      <div className={styles.grid}>
+      <div className={formStyles.grid}>
         <Form.Input
           label={texts.register.fields.country}
           type='text'
           name='country'
+          {...register('country')}
+          error={errors.country?.message}
         />
         <Form.Input
           label={texts.register.fields.state}
           type='text'
           name='state'
+          {...register('state')}
+          error={errors.state?.message}
         />
         <Form.Input
           label={texts.register.fields.city}
           type='text'
           name='city'
+          {...register('city')}
+          error={errors.city?.message}
         />
         <Form.Select
           label={texts.register.fields.gender}
           name='gender'
+          {...register('gender')}
+          error={errors.gender?.message}
           options={genderOptions}
-          placeholder={texts.register.genderOptions.placeholder}
         />
       </div>
       <Form.Radio
         label={texts.register.fields.modality}
         name='modality'
+        {...register('modality')}
+        error={errors.modality?.message}
         options={modalityOptions}
       />
-    </section>
+    </form>
   );
 }
