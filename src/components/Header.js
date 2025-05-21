6b28f0fc03a4858db5bf5b28ee6682b1e109c2d5
Jessica Viana <v.jessi.carvalho@gmail.com>
2025-05-21 11:26:19 -0300

    alterações no container do logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import Link from 'next/link';
-import { useLanguage } from '../contexts/LanguageContext';
+import Image from "next/image";
+import Link from "next/link";
+import { useLanguage } from "../contexts/LanguageContext";
 
 export default function Header() {
   const { texts, toggleLanguage, language } = useLanguage();
@@ -10,11 +10,21 @@ export default function Header() {
   return (
     <header className="header">
       <div className="logo-container">
-        <Link href="/">
-          <Image src="/fishbase-logo.png" alt="FishBase" width={160} height={40} />
+        <Link href="https://www.fishbase.org">
+          <Image
+            src="/fishbase-logo.png"
+            alt="FishBase"
+            width={190}
+            height={42}
+          />
         </Link>
-        <Link href="/">
-          <Image src="/sealifebase-logo.png" alt="SeaLifeBase" width={160} height={40} />
+        <Link href="https://sealifebase.org/">
+          <Image
+            src="/sealifebase-logo.png"
+            alt="SeaLifeBase"
+            width={210}
+            height={46}
+          />
         </Link>
       </div>
       <div className="nav-container">
@@ -26,14 +36,14 @@ export default function Header() {
         </nav>
         <div className="lang-switch">
           <button
-            onClick={() => toggleLanguage('en')}
-            className={language === 'en' ? 'active' : ''}
+            onClick={() => toggleLanguage("en")}
+            className={language === "en" ? "active" : ""}
           >
             EN
           </button>
           <button
-            onClick={() => toggleLanguage('pt')}
-            className={language === 'pt' ? 'active' : ''}
+            onClick={() => toggleLanguage("pt")}
+            className={language === "pt" ? "active" : ""}
           >
             PT
           </button>
