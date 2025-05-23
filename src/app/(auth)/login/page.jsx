2e221cdcb98b1d1f09f8a19d977473b4d696f5b4
Davi Bittencourt <dbittencourt2002@gmail.com>
2025-05-22 22:09:41 -0300

    feat(login): adiciona página de login do usuário

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,14 +1,107 @@
 'use client';
 
+import styles from '@/app/(auth)/layout.module.css';
+
 import { useAuthLayout } from '@/contexts/AuthLayoutContext';
-import { useEffect } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+import { useLanguage } from '@/contexts/LanguageContext';
+import { Form } from '@/components/Form';
+import { createLoginSchema } from '@/schemas/login.schema';
+import { useFormStage } from '@/hooks/useFormStage';
+import { AtSign, Lock } from 'lucide-react';
+import PasswordIcon from '@/components/PasswordIcon';
+import Button from '@/components/Button';
+import { login } from '@/lib/api';
+import { useRouter } from 'next/navigation';
 
 export default function LoginPage() {
-  const { setBannerImage } = useAuthLayout();
+  const { setBannerImage, setIsLoading, setErrorMessage } = useAuthLayout();
+  const { texts, language } = useLanguage();
+
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const router = useRouter();
+
+  const handlePasswordVisibility = () => {
+    setIsPasswordVisible((prev) => !prev);
+  };
+
+  const defaultValues = {
+    email: '',
+    password: '',
+  };
+
+  const schema = useMemo(() => createLoginSchema(language), [language]);
+
+  const { register, errors, getValues } = useFormStage(schema, defaultValues);
 
   useEffect(() => {
     setBannerImage('/register-banner.png');
   }, []);
 
-  return null;
+  const handleLogin = async () => {
+    setIsLoading(true);
+
+    const data = getValues();
+
+    const response = await login(data.email, data.password);
+
+    if (response.success) {
+      setIsLoading(false);
+      router.push('/');
+      return;
+    }
+
+    setErrorMessage(response.error.message);
+
+    setIsLoading(false);
+  };
+
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>{texts.login.title}</h1>
+      </header>
+      <div
+        className={styles.form}
+        style={{ justifyContent: 'normal', gap: '4.2rem' }}
+      >
+        <form className={styles.stage}>
+          <Form.Input
+            label={texts.register.fields.email}
+            type='text'
+            name='email'
+            {...register('email')}
+            error={errors.email?.message}
+            leftIcon={<AtSign size={24} color='#5F84A7' />}
+          />
+          <Form.Input
+            label={texts.register.fields.password}
+            type={isPasswordVisible ? 'text' : 'password'}
+            name='password'
+            {...register('password')}
+            error={errors.password?.message}
+            leftIcon={<Lock size={24} color='#5F84A7' />}
+            rightIcon={
+              <PasswordIcon
+                isVisible={isPasswordVisible}
+                setIsVisible={handlePasswordVisibility}
+                color='#5F84A7'
+              />
+            }
+          />
+        </form>
+
+        <div className={styles.buttons}>
+          <Button
+            variant='filled'
+            style={{ width: '100%', paddingBlock: '1rem' }}
+            onClick={handleLogin}
+          >
+            <span>{texts.login.button}</span>
+          </Button>
+        </div>
+      </div>
+    </>
+  );
 }
