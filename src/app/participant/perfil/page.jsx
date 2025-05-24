321ac0a32adfef1fe4a4f108039fa01548e09535
Davi Bittencourt <dbittencourt2002@gmail.com>
2025-05-24 14:08:07 -0300

    feat: adiciona componente de perfil na página

--- src/app/participant/perfil/page.jsx
+++ src/app/participant/perfil/page.jsx
@@ -1,12 +1,12 @@
 'use client';
 
-import Image from 'next/image';
+import User from '@/components/UserComponents/User';
 import styles from './page.module.css';
 
 export default function Page() {
   return (
     <div className={styles.pageCard}>
-      <h1>perfil</h1>
+      <User />
     </div>
   );
 }
