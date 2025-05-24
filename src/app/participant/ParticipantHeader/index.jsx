a85a82e6e05e3991e51ea1c94b17001f30431a46
Davi Bittencourt <dbittencourt2002@gmail.com>
2025-05-24 14:06:28 -0300

    refactor: utiliza componente de switch na página

--- src/app/participant/ParticipantHeader/index.jsx
+++ src/app/participant/ParticipantHeader/index.jsx
@@ -2,42 +2,27 @@
 
 import styles from './ParticipantHeader.module.css';
 import Image from 'next/image';
-import { useLanguage } from '../../../contexts/LanguageContext';
+import LanguageSwitch from '@/components/LanguageSwitch';
 
 export function ParticipantHeader() {
-  const { language, toggleLanguage } = useLanguage();
-
   return (
     <header className={styles.header}>
       <div className={styles.logoContainer}>
         <Image
-          src="/fishbase-logo.png"
-          alt="Logo FishBase"
+          src='/fishbase-logo.png'
+          alt='Logo FishBase'
           width={90}
           height={30}
         />
         <Image
-          src="/sealifebase-logo.png"
-          alt="Logo SeaLifeBase"
+          src='/sealifebase-logo.png'
+          alt='Logo SeaLifeBase'
           width={110}
           height={30}
         />
       </div>
 
-      <div className={styles.languageSwitch}>
-        <button
-          className={language === 'en' ? styles.active : ''}
-          onClick={() => toggleLanguage('en')}
-        >
-          EN
-        </button>
-        <button
-          className={language === 'pt' ? styles.active : ''}
-          onClick={() => toggleLanguage('pt')}
-        >
-          PT
-        </button>
-      </div>
+      <LanguageSwitch color='blue' />
     </header>
   );
 }
