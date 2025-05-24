de7d1ecac7b405535e07a82ac3a67e4349113683
Davi Bittencourt <dbittencourt2002@gmail.com>
2025-05-24 14:13:14 -0300

    fix: remove estilização desnecessária

--- src/app/participant/perfil/page.jsx
+++ src/app/participant/perfil/page.jsx
@@ -1,12 +1,7 @@
 'use client';
 
 import User from '@/components/UserComponents/User';
-import styles from './page.module.css';
 
 export default function Page() {
-  return (
-    <div className={styles.pageCard}>
-      <User />
-    </div>
-  );
+  return <User />;
 }
