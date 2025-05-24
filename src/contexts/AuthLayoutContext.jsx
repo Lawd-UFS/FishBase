b41c727c8a4d82ce288be781e9c52c8973b8e27c
Davi Bittencourt <dbittencourt2002@gmail.com>
2025-05-24 12:28:29 -0300

    refactor: remove funções de loading e erros globais

--- src/contexts/AuthLayoutContext.jsx
+++ src/contexts/AuthLayoutContext.jsx
@@ -6,23 +6,12 @@ const AuthLayoutContext = createContext(null);
 
 export const AuthLayoutProvider = ({ children }) => {
   const [bannerImage, setBannerImage] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState(null);
-
-  const resetErrorMessage = useCallback(() => {
-    setErrorMessage(null);
-  }, []);
 
   return (
     <AuthLayoutContext.Provider
       value={{
         bannerImage,
         setBannerImage,
-        isLoading,
-        setIsLoading,
-        errorMessage,
-        setErrorMessage,
-        resetErrorMessage,
       }}
     >
       {children}
